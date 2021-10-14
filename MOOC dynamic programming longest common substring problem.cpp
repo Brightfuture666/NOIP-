f0a4f33a236c15360453371c50c7fee99dbed4f0
Brightfuture666 <75941511+Brightfuture666@users.noreply.github.com>
2021-10-14 22:56:43 +0800

    Add files via upload

---
+++ MOOC dynamic programming longest common substring problem.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m, f[205][205] = {0}, ans = 0, cnt = 0, l = 0, l_max = 0;
+	char a[205] = {'0'}, b[205] = {'0'};
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 1; i <= m; i++) {
+		cin >> b[i];
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (a[i] != b[j]) {
+				f[i][j] = 0;
+				if (l > l_max) {
+					l_max = l;
+				}
+				l = 0;
+			} else {
+				f[i][j] = f[i - 1][j - 1] + 1; ans = max(ans, f[i][j]);
+				if (f[i][j] >= ans) {
+					cnt = i;
+				}
+				l++;
+			}
+		}
+	}
+	cout << ans << endl;
+	char Ans[205] = {'0'}; int k = 0;
+	while (l_max)  {
+		Ans[++k] = a[cnt];
+		cnt--; l_max--;
+	}
+	for (int i = 1; i <= k; i++) {
+		cout << Ans[i] << " ";
+	}
+	return 0;
+}
